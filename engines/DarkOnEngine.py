1f83f8771dc8cc44ed2c227d67e6db9da1e0f9a6
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-03-20 10:26:27 +0100

    Update DarkOnEngine.py

--- engines/DarkOnEngine.py
+++ engines/DarkOnEngine.py
@@ -83,7 +83,7 @@ def calculate_think_time(remaining_time_ms):
     elif t >= 5:
         return rnd.uniform(0.1, 1.5)
     else:
-        return 0.1    # panic
+        return 0.0   # panic
 
 
 def fast_board_key(board: chess.Board):
